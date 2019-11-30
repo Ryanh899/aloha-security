8a47ed3cb3e5c8cf5f52e2a05b80fc6c14ed20d5
Ryan <rhazzardphoto@gmail.com>
2019-11-29 16:03:14 -0800

    sticky menu

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const path = require('path'); 
 
-const PORT = process.env.PORT || 3000; 
+const PORT = process.env.PORT || 3001; 
 
 // send static content 
 app.use(express.static(path.join(__dirname + '/views/'))); 
